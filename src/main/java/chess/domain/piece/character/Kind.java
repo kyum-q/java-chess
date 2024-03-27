2680c1a8dcad2af0de52d077d090c0db9b09e930
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-03-27 11:20:52 +0900

    refactor(Kind): 체스 기물 별로 점수(최고 점수, 최저 점수)가 있다.

--- src/main/java/chess/domain/piece/character/Kind.java
+++ src/main/java/chess/domain/piece/character/Kind.java
@@ -1,11 +1,27 @@
 package chess.domain.piece.character;
 
 public enum Kind {
-    PAWN,
-    KNIGHT,
-    BISHOP,
-    ROOK,
-    QUEEN,
-    KING,
+    PAWN(1.0, 0.5),
+    KNIGHT(2.5, 2.5),
+    BISHOP(3.0, 3.0),
+    ROOK(5.0, 5.0),
+    QUEEN(9.0, 9.0),
+    KING(0.0, 0.0),
     ;
+
+    private final double maxScore;
+    private final double minScore;
+
+    Kind(double maxScore, double minScore) {
+        this.maxScore = maxScore;
+        this.minScore = minScore;
+    }
+
+    public double maxScore() {
+        return maxScore;
+    }
+
+    public double minScore() {
+        return minScore;
+    }
 }
