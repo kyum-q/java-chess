8b459595bee9b1af4849f1ffc1474f0a8991e9ea
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-03-28 16:59:02 +0900

    feat(ConnectionGenerator): DB 연결해주는 클래스 구현

---
+++ src/test/java/chess/dao/ConnectionGeneratorTest.java
@@ -0,0 +1,22 @@
+package chess.dao;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConnectionGeneratorTest {
+
+    @DisplayName("DB와 연결된 Connection을 얻을 수 있다.")
+    @Test
+    void getConnection() {
+        ConnectionGenerator generator = new ConnectionGenerator();
+        try (final var connection = generator.getConnection()) {
+            assertThat(connection).isNotNull();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
