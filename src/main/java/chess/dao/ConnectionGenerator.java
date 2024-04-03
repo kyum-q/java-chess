9f0d05a3ea75cb220a72e81c98f2a560023890d4
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-04-03 13:51:12 +0900

    refactor(ConnectionGenerator): 사용하는 server 설정 값 server_config.yaml 로 작성

--- src/main/java/chess/dao/ConnectionGenerator.java
+++ src/main/java/chess/dao/ConnectionGenerator.java
@@ -1,14 +1,40 @@
 package chess.dao;
 
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Map;
 
 public class ConnectionGenerator {
-    private static final String SERVER = "localhost:3306"; // MySQL 서버 주소
-    private static final String OPTION = "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "root"; //  MySQL 서버 아이디
-    private static final String PASSWORD = "root"; // MySQL 서버 비밀번호
+    private static final String SERVER; // MySQL 서버 주소
+    private static final String OPTION;
+    private static final String USERNAME; //  MySQL 서버 아이디
+    private static final String PASSWORD; // MySQL 서버 비밀번호
+
+    static {
+        // YAML 파일 경로
+        String yamlFilePath = "src/main/resources/server_config.yaml";
+
+        // YAML 파일을 FileInputStream으로 읽기
+        try (final var inputStream = new FileInputStream(yamlFilePath)) {
+
+            // SnakeYAML을 사용하여 YAML 파싱
+            Yaml yaml = new Yaml();
+            Map<String, Object> yamlMap = yaml.load(inputStream);
+            Map<String, String> resultMap = (Map<String, String>) yamlMap.get("Server");
+
+            SERVER = resultMap.get("address");
+            OPTION = resultMap.get("option");
+            USERNAME = resultMap.get("user_name");
+            PASSWORD = resultMap.get("password");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private ConnectionGenerator() {
     }
